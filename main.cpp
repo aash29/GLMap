7e4862c033967aa9e8922dd8710cdcd719228f86
aash29 <aash29@gmail.com>
2017-08-01 13:42:24 +0300

    asdas

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main()
 	float vertices[] = {
 		0.0f,  0.5f, // Vertex 1 (X, Y)
 		0.5f, -0.5f, // Vertex 2 (X, Y)
-		-0.5f, -0.5f  // Vertex 3 (X, Y)
+		-0.5f, -0.5f,  // Vertex 3 (X, Y)
 	};
 
 	GLuint vao;
