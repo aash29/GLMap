b2257e4c0a7a1377389d3a113aba2300061bc22f
aash29 <aash29@gmail.com>
2017-08-28 17:08:02 +0300

    sadfsd

--- map.hpp
+++ map.hpp
@@ -1,12 +1,138 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include "tesselator.h"
+
+
 struct building
 {
   std::string id;
   std::string name;
-  std::string number;
+  std::string addrNumber;
+  std::string addrStreet;
   std::vector<std::vector<float> > coords;
 };
+
+float xmin,xmax,ymin,ymax;
+
+void loadLevel(const char *name,TESStesselator* tess)
+{
+  
+  
+  std::string m_currentLevel = std::string(name);
+  
+
+  nlohmann::json jsonObj;
+
+  std::ifstream file;
+  file.open(std::string(name), std::ios::in);
+  if (file) {
+    printf("file open \n");
+    jsonObj << file;
+    file.close();
+  }
+
+  auto f1 = jsonObj.find("features");
+
+
+  std::map<std::string, building> m3;
+
+  
+  if (f1 != jsonObj.end()) {
+    //m_force = m_forceLeft;
+    std::cout << "found features";
+    //std::cout << ((*f1)[0]).dump(4);
+
+
+	std::map<std::string, float**> m2;
+	float** a2;
+	float* a1[10];
+
+
+	xmin = 30.2882633f;
+	xmax = 30.2882633f;
+	ymin = 59.9379525f;
+	ymax = 59.9379525f;
+
+    for (nlohmann::json::iterator it = (*f1).begin(); it != (*f1).end(); ++it) {
+      if (((*it)["properties"]).find("building") != ((*it)["properties"]).end()) {
+		std::cout << "id:" << (*it)["properties"]["id"] << "\n";
+		//std::cout <<  (*it)["geometry"]["type"];
+		
+		if ((*it)["geometry"]["type"]=="Polygon"){
+
+		std::vector< std::vector<std::vector<float> > > c1 =  (*it)["geometry"]["coordinates"];
+
+
+		a2 = new float*[c1.size()];
+		std::string id = (*it)["properties"]["id"];
+		
+		m3[id].coords=std::vector <std::vector <float> >();
+		m3[id].id = id;
+
+		if (((*it)["properties"]).find("name") != ((*it)["properties"]).end())
+		  m3[id].name = (*it)["properties"]["name"];
+		if (((*it)["properties"]).find("addr:street") != ((*it)["properties"]).end())
+		  m3[id].addrStreet = (*it)["properties"]["addr:street"];
+		if (((*it)["properties"]).find("addr:housenumber") != ((*it)["properties"]).end())
+		m3[id].addrNumber = (*it)["properties"]["addr:housenumber"];
+		
+
+		for (int j = 0; j<c1.size();j++){
+
+		  a2[j] = new float[c1[j].size()*2];
+		  m3[id].coords.push_back(std::vector<float>());
+		  
+		  std::cout << "contour size:" << c1[j].size() << "\n";
+	  
+		  for (int i = 0; i<c1[j].size();i++){
+		    //a2[j][2*i]=c1[j][i][0];
+		    //	a2[j][2*i+1]=c1[j][i][1];
+
+			m3[id].coords[j].push_back(c1[j][i][0]);
+			m3[id].coords[j].push_back(c1[j][i][1]);
+			
+	    
+			xmin=std::min(xmin,a2[j][2*i]);
+			xmax=std::max(xmax,a2[j][2*i]);
+	    
+			ymin=std::min(ymin,a2[j][2*i+1]);
+			ymax=std::max(ymax,a2[j][2*i+1]);
+		  };
+		  std::cout << "adding contour" << "\n";
+		}
+		
+		//m2[(*it)["properties"]["id"]] = a2;
+		/*
+		for (int j = 0; j<c1.size();j++){
+
+		  tessAddContour(tess, 2, m3[id].coords[j].data(), sizeof(float) * 2, c1[j].size());
+		  };*/
+		
+		  //free(a1);
+		};
+
+	
+	//std::cout << c1 << '\n';
+      }
+      /*
+      if ((*it)["properties"]["type"]=="route"){
+	std::cout << *it << '\n';
+      }
+      */
+    }
+  }
+
+      for (auto &it : m3) {
+	for (int j = 0; j < it.second.coords.size(); j++) {
+	  for (int i = 0; i < it.second.coords[j].size();i=i+2){
+	    it.second.coords[j][i] = (it.second.coords[j][i]-xmin)/(xmax-xmin);
+	    it.second.coords[j][i+1] = (it.second.coords[j][i+1]-ymin)/(ymax-ymin);
+	  }		  
+	  tessAddContour(tess, 2, it.second.coords[j].data(), sizeof(float) * 2, round(it.second.coords[j].size()/2));
+	}
+      }
+
+}
 
 #endif
