8b62e601dc655732f544ba4e779139985910fff7
aash29 <aash29@gmail.com>
2018-06-14 11:35:16 +0300

    asdad

--- plan.py
+++ plan.py
@@ -5,6 +5,9 @@ utime=0;  #universal time
 dtime=0;  # day time
 day=0;
 
+def dayTime():
+    return (utime%96);
+
 distToWork=1;
 distToShop=1;
 workDifficulty=1;
@@ -15,7 +18,7 @@ workDuration=24;
 sat=32;
 heatHome=0;
 inv=['foodStamp','foodStamp'];
-invHome=['wood'];
+invHome=['wood','food'];
 energy=256;
 absent=0;
 workDays=0;
@@ -26,12 +29,22 @@ loc="atHome"; #atWork, atHome, underway
 
 
 def eatPre():
-    return (loc=="atHome")&('foodStamp' in inv)
+    return (loc=="atHome")&('food' in invHome)
 
 def eatEff():
     sat=32;
     utime+=1;
 
+def gotoShopPre():
+    return (loc=="atHome")&('foodStamp' in inv)
+
+def gotoShopEff():
+    inv.remove('foodStamp')
+
+def goal():
+    return (dayTime()>90)
+
+    
 print(eatPre())
 
 
