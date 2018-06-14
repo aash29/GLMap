6f9eff29f1fe3a747042925d01ecfeabfbd13c95
aash29 <aash29@gmail.com>
2018-06-15 00:46:30 +0300

    awdwad

--- plan.py
+++ plan.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
+
 import random
-
-
-
+import copy
 
 distToWork=1;
 distToShop=1;
@@ -13,26 +12,27 @@ workDuration=24;
 
 
 class stateClass:
-    pass
+    def __hash__(self):
+        return hash((self.__dict__.values()))
+    def __str__(self):
+        return 'energy: '+ str(self.energy) +',' + 'utime:' + str(self.utime) 
 
-state = stateClass();
+    
+state0 = stateClass();
 
 
-state.sat=32;
-state.heatHome=0;
-state.inv=['foodStamp','foodStamp'];
-state.invHome=['wood','food'];
-state.energy=256;
-state.absent=0;
-state.workDays=0;
-state.isHomeHeated=0;
-#foodStamps=random.randint(5,10);
-#wood=random.randint(1,5);
-state.loc="atHome"; #atWork, atHome, underway
-state.utime=0;  #universal time
-#dtime=0;  # day time
-#day=0;
-state.dayTime=lambda: state.utime%96;
+state0.sat=32;
+state0.heatHome=0;
+state0.inv=['foodStamp','foodStamp'];
+state0.invHome=['wood','food'];
+state0.energy=256;
+state0.absent=0;
+state0.workDays=0;
+state0.isHomeHeated=0;
+state0.loc="atHome"; #atWork, atHome, underway
+state0.utime=0;  #universal time
+state0.dayTime=lambda: state.utime%96;
+
 #def dayTime():
 #    return (utime%96);
 
@@ -48,74 +48,102 @@ state.dayTime=lambda: state.utime%96;
 
 class eat():
     duration = 1;
+    name = 'eat'
     #@staticmethod
-    def pre():
+    def pre(state):
         #result = super(eat,eat).pre();
-        result = (energy>0)&(sat>0)&(loc == "atHome")&('food' in invHome);
+        result = (state.energy>0)&(state.sat>0)&(state.loc == "atHome")&('food' in state.invHome);
         return result;
     #@staticmethod
     def eff(state):
         #global utime
         #super(eat,eat).eff();
-        state.utime += eat.duration;
-        state.sat=32;
-        print ('eating');
-        return state;
+        s1 = copy.deepcopy(state);
+        s1.utime += eat.duration;
+        s1.sat=32;
+        s1.energy -= eat.duration;
+        s1.invHome.remove('food')
+        #print ('eating');
+        return s1;
         
     
 class gotoShop():
+    name = 'go to shop'
     duration = 2*distToShop + 1
     #@staticmethod
-    def pre():
+    def pre(state):
         #result = super(gotoShop,gotoShop).pre();
-        return (energy>0)&(sat>0)&(loc=="atHome")&('foodStamp' in inv)
-    @staticmethod
+        return (state.energy>0)&(state.sat>0)&(state.loc=="atHome")&('foodStamp' in state.inv)
+    #@staticmethod
     def eff(state):
+        s1 = copy.deepcopy(state);
         #super(gotoShop,gotoShop).eff();
-        state.inv.remove('foodStamp')
-        print ('goind to Shop');
-        return state;
+        s1.inv.remove('foodStamp')
+        #print ('going to Shop');
+        s1.energy -= gotoShop.duration;
+        return s1;
 
 class wait():
+    name = 'wait'
     duration = 1;
-    def pre():
-        return True;
+    #def __str__():
+    #    return 'wait'
+    def pre(state):
+        return (state.energy>0)&(state.sat>0);
     def eff(state):
-        state.utime += wait.duration;
-        print('Waiting');
-        return state;
+        s1 = copy.deepcopy(state);
+        s1.utime += wait.duration;
+        s1.energy -= wait.duration;
+        #print('Waiting');
+        return s1;
 
     
 
 def goal(state):
-    return (state.dayTime()>90)&(state.energy>0)&(state.sat>0)
+    return (state.utime>90)&(state.energy>0)&(state.sat>0)
 
 
 actions = [eat, gotoShop, wait];
 
-start = state
+start = state0
 
 
-stack=[(start,[start])];
-visited = set(state);
+stack=[(start,[])];
+visited = set([start]);
 
-while stack:
-    state, path = 
-    for a1 in actions:
-        s1 = a1.eff(state);
-        if (a1.pre()) and (not s1 in visited):
-            stack.append((s1, );
-            visited.add(s1);
+
+def planDay():
+    while stack:
+        s0, path = stack.pop()
+        print(s0)
+        #print(path)
+        
+        for a1 in actions:
+            if (a1.pre(s0)):
+                s1 = a1.eff(s0);
+                if goal(s1):
+                    plan = path + [a1]
+                    #state = s1;
+                    return s1, plan
+                
+            #print(s1 in visited)
+        # and (not s1 in visited):
+        stack.append((s1,path + [a1]));
+        visited.add(s1);
+
+s1, plan = planDay();
+
+
+    
+print(plan)
+print(s1)
+    #print(stack)
+
             
     #print("Next action:");
     #a1 = random.choice(actions);
     
     #    a1.eff(state);
     
-
-
-
-    
-print(eatPre())
 
 
